3307b9f2710af903ae2621dec5dad46988b945d3
Kavya Hanisha <gajula-uma-venkata.k@husky.neu.edu>
2019-03-03 10:27:05 -0500

    hp

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
     $(function(){
         $("#profile-card-header").typed({
-            strings: ["CS Graduate'19", "Software Developer", "Confidence and Pride","Technology Optimist"],
+            strings: ["CS Graduate'19", "Software Developer", "Confident and Happy ","Technology Optimist"],
             typeSpeed: 0.4,
             loop: true
         });
